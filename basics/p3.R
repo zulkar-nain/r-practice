4a778ccac4fa1be4d25a9823f968711540b2e945
zulkar-nain <zulkarnainlabrotorian@gmail.com>
2026-08-26 23:53:09 +0600

    beginner problems p3

---
+++ basics/p3.R
@@ -0,0 +1,13 @@
+#VectorMathAndStatistics
+
+exam_scores <- c(85, 90, 78, 92, 88)
+
+max_score <- max(exam_scores)
+
+min_score <- min(exam_scores)
+
+mean_score <- mean(exam_scores)
+
+print(paste("Maximum Score:", max_score))
+print(paste("Minimum Score:", min_score))
+print(paste("Mean Score:", mean_score))
