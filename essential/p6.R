894a5c8f46ade11245d2e09281910107cc0a9a56
zulkar-nain <zulkarnainlabrotorian@gmail.com>
2026-09-06 23:47:26 +0600

    essential  problems p6

---
+++ essential/p6.R
@@ -0,0 +1,22 @@
+#Custom Functions and Categorization
+
+
+customer_sales <- data.frame(
+  customer = c("Alice", "Bob", "Charlie", "Diana", "Evan"),
+  sales = c(150, 450, 600, 220, 90)
+)
+
+classify_customer <- function(amount) {
+  if (amount >= 400) {
+    return("VIP")
+  } else if (amount >= 200) {
+    return("Regular")
+  } else {
+    return("New")
+  }
+}
+
+customer_sales$tier <- sapply(customer_sales$sales, classify_customer)
+
+# 4. Print the final updated data frame
+print(customer_sales)
